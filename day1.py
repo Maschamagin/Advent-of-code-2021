519c0ceda16bd2c85bce1e00ce3a14ac74bdaf27
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2022-03-14 16:14:25 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -8,7 +8,7 @@ https://adventofcode.com/2021/day/1
 """
 input_file = '/path/to/day1/input.txt'
 
-
+#---------------- part 1 --------------------------
 def count_depth_increasements(input):
     # Count for number of times the depth of sonar increases - will be returned later:
     depth_increase_count = 0
@@ -28,4 +28,32 @@ def count_depth_increasements(input):
     # First element in list is always greater than first_value at first assignment (which is 0). Therefore, return count - 1:
     return depth_increase_count - 1
 
+
+# ----------------------- Part 2 --------------------------------
+def count_depth_increasment_shifting_window(input, windowsize):
+    sum_of_entries_in_shifting_window = []
+    depth_increase_count_shifting_window = 0
+
+    with open(input,'r') as puzzle:
+        lines = puzzle.readlines()
+    for k in  range (0, len(lines)):
+        lines[k] = int(lines[k]) 
+
+    sum = 0
+    for i in range(len(lines) - windowsize + 1 ):
+        for j in range (0, windowsize):
+            sum += lines[i+j]
+        sum_of_entries_in_shifting_window.append(sum)
+        sum = 0
+    
+    depth_increase_count = 0
+    first_value = 0  
+
+    for elem in range (0, len(sum_of_entries_in_shifting_window)):
+        if(sum_of_entries_in_shifting_window[elem] > first_value):
+            depth_increase_count += 1
+        first_value = sum_of_entries_in_shifting_window[elem] 
+    return depth_increase_count - 1
+
 print(count_depth_increasements(input_file))
+print(count_depth_increasment_shifting_window(input_file, 3))
