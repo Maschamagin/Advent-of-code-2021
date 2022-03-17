2dd8153a784046bcd668af88a735079d079dcd89
t14 <mascha.magin@gmail.com>
2022-03-17 19:06:56 +0100

    edit day2.py

--- day2.py
+++ day2.py
@@ -7,11 +7,10 @@ https://adventofcode.com/2021/day/1
 -------------------------------------------------------------
 """
 
-input_file = 'day2/input.txt'
 
-
+# -------------------------- Part 1 -------------------------------------
 def position(input_file):
-    #open file and 
+    # open file and 
     with open(input_file,'r') as puzzle:
         input_lines = puzzle.readlines()
 
@@ -20,7 +19,9 @@ def position(input_file):
     sum_up = 0
 
     for line in input_lines:
-        # create tmp list for every line in the file, first value is the command, second value is the steps
+        # create tmp list for every line in the file, first value is the direction, second value is the steps
+        # e.g. the line 'forward 3' of the input file becomes the list ['forward', 3]. 
+        # input_lines is now a two-dimensional list: [['direction0', x], ['direction1', y] ...]:
         tmp = [s.replace(' ', '') for s in line.strip().split(' ')]
 
         # now add up all steps in the different directions
@@ -37,4 +38,45 @@ def position(input_file):
     # return the product of horizontal and depth position. That is the solution to the puzzle
     return horizontal_position*depth_position
 
-print(position(input_file))    
+
+# -------------------------- Part 2 -------------------------------------
+def position2(input_file):
+    # open file and 
+    with open(input_file,'r') as puzzle:
+        input_lines = puzzle.readlines()
+
+    sum_forward = 0
+    sum_down = 0
+    sum_up = 0
+    aim = 0
+    depth = 0
+
+    for line in input_lines:
+        # create tmp list for every line in the file, first value is the direction, second value is the steps
+        # e.g. the line 'forward 3' of the input file becomes the list ['forward', 3]. 
+        # input_lines is now a two-dimensional list: [['direction0', x], ['direction1', y] ...]:
+        tmp = [s.replace(' ', '') for s in line.strip().split(' ')]
+
+        # now add up all steps in the different directions
+        if(tmp[0] == 'forward'):
+            sum_forward += int(tmp[1])
+            depth += (int(tmp[1]) * aim)
+        elif(tmp[0] == 'down'):
+            aim += int(tmp[1])
+        else:
+            aim -= int(tmp[1])
+
+    horizontal_position = sum_forward
+    depth_position = depth
+
+    # return the product of horizontal and depth position. That is the solution to the puzzle
+    return horizontal_position*depth_position
+
+def main():
+    input_file = '/home/masch/Dokumente/Coding/Advent of Code/2021/day2/input.txt'
+    return_value = position(input_file)
+    print(position(input_file))
+    print(position2(input_file))
+
+if __name__ == '__main__':
+    main() 
