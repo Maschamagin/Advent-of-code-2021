4ce8dd8568ad13497a03fc27ee62d39dd5d7ef8c
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2022-04-28 11:31:41 +0200

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,66 +1,71 @@
 """
 -------------------------------------------------------------
-Solution for Advent of Code 2021 Day 1: Sonar Sweep. 
+Solution for Advent of Code 2021 Day 1: Sonar Sweep.
 
-For the complete puzzle description and to get your own input go to: 
+For the complete puzzle description and to get your own input go to:
 https://adventofcode.com/2021/day/1
 -------------------------------------------------------------
 """
 
 # ---------------------- Part 1 ----------------------------------------
-def count_depth_increasements(input, lines):
-    # Count for number of times the depth of sonar increases - will be returned later:
+def count_depth_increasements(lines):
+    '''
+    Function takes a list as argument.
+    Count for number of times depth increases will be returned.
+    '''
     depth_increase_count = 0
     first_value = 0
 
     # compare each item of list with the one before, update depth_increase_count:
     for line in lines:
-        if(int(line.strip()) > first_value):
+        if int(line.strip()) > first_value:
             depth_increase_count += 1
         first_value = int((line.strip()))
 
-    # First element in list is always greater than first_value at first assignment (which is 0). 
-    # Therefore, return count - 1:
+    # Return count -1 bc first element in list is always greater at first assignment (which is 0).
     return depth_increase_count - 1
 
-
-
 # ----------------------- Part 2 --------------------------------
-def count_depth_increasment_shifting_window(input, windowsize):
-    
-    # read the file and store items in list, store integer value as each element
-    with open(input,'r') as puzzle:
+def count_depth_increasment_shifting_window(input_file, windowsize):
+    '''
+    Function takes a file as argument.
+    Reads the file and stores items in list, stores integer value as each element.
+    Count (int) for number of times the depth of sonar increases will be returned.
+    '''
+    with open(input_file,'r') as puzzle:
         lines = puzzle.readlines()
     for k in  range (0, len(lines)):
-        lines[k] = int(lines[k]) 
+        lines[k] = int(lines[k])
 
-    sum = 0
+    sum_tmp = 0
     sum_of_entries_in_shifting_window = []
 
     # Create shifting window with windowsize as parameter
     for i in range(len(lines) - windowsize + 1 ):
         for j in range (0, windowsize):
-            sum += lines[i+j]
+            sum_tmp += lines[i+j]
         sum_of_entries_in_shifting_window.append(sum)
-        sum = 0
-    
-    # Now basically repeat procedure from part 1: 
-    # Compare values in a list and see how many times current value is greater than value at index before
+        sum_tmp = 0
+
+    # Check how many times current value is greater than value at index before
     depth_increase_count = 0
-    first_value = 0  
+    first_value = 0
 
     for elem in range (0, len(sum_of_entries_in_shifting_window)):
-        if(sum_of_entries_in_shifting_window[elem] > first_value):
+        if sum_of_entries_in_shifting_window[elem] > first_value:
             depth_increase_count += 1
-        first_value = sum_of_entries_in_shifting_window[elem] 
+        first_value = sum_of_entries_in_shifting_window[elem]
     return depth_increase_count - 1
 
 def main():
+    '''
+    Main function to solve the puzzle
+    '''
     input_file = 'day3/input_day3.txt'
     with open(input_file,'r') as puzzle:
         input_lines = puzzle.readlines()
-    print(count_depth_increasements(input_file, input_lines))
+    print(count_depth_increasements(input_lines))
     print(count_depth_increasment_shifting_window(input_file, 3))
 
 if __name__ == '__main__':
-    main() 
+    main()
